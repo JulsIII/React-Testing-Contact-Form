60c899c440241f2001365704783fbb507ed2a4cd
Your Name <you@example.com>
2021-01-14 15:54:30 -0600

    2 - MVP Met, tests work

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import ContactForm from './ContactForm';
 
-test("renders without errors", async () => {
+test("renders form without errors", async () => {
     //Arrage
     render(<ContactForm/>);
 
     //Act:
-    //  1. Get our species, age and notes input fields.
-    // const speciesInput = screen.getByLabelText(/species/i);
-    // const ageInput = screen.getByLabelText(/age/i);
-    // const notesInput = screen.getByLabelText(/notes/i);
+    //  1. Get our input fields.
+    const fNameInput = screen.getByLabelText(/first name/i);
+    const lNameInput = screen.getByLabelText(/last name/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const msgInput = screen.getByLabelText(/message/i);
 
     //  2. Type values into our input fields.
-    // userEvent.type(speciesInput, "Grizzly Bear");
-    // userEvent.type(ageInput, "33");
-    // userEvent.type(notesInput, "the cutest!!!!!!");
+    userEvent.type(fNameInput, "Bob");
+    userEvent.type(lNameInput, "Bobblin");
+    userEvent.type(emailInput, "bob@bobs.com");
+    userEvent.type(msgInput, "Bob Bobblin approves of the message!");
 
     //  3. Push the submit button.
-    // const button = screen.getByRole('button');
-    // userEvent.click(button);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
     
     //Assert:
-    // Species name is on the screen.
-    // const newAnimal = await screen.findByText("Grizzly Bear");
-    // expect(newAnimal).toBeInTheDocument();
+    // Info is on the screen.
+    const newfName = await screen.findByDisplayValue("Bob");
+    const newlName = await screen.findByDisplayValue("Bobblin"); 
+    const newEmail = await screen.findByDisplayValue("bob@bobs.com");  
+    const newMsg = await screen.findByDisplayValue("Bob Bobblin approves of the message!");
+    expect(newfName).toBeInTheDocument();
+    expect(newlName).toBeInTheDocument();
+    expect(newEmail).toBeInTheDocument();
+    expect(newMsg).toBeInTheDocument();
 });
